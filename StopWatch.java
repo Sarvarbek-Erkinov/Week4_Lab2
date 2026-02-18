cf2b9848df89f94c8ab756b0e2e7bfa6cd2820fe
Sarvarbek-Erkinov <250421@newuu.uz>
2026-02-18 13:36:03 +0500

    Create StopWatch.java

---
+++ StopWatch.java
@@ -0,0 +1,28 @@
+import java.time.LocalTime;
+
+public class StopWatch {
+    private long startTime;
+
+    private long endTime;
+
+    public long getStartTime(){return startTime;}
+
+    public long getEndTime(){return endTime;}
+
+    public StopWatch(){
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public void start(){
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public void stop(){
+        this.endTime = System.currentTimeMillis();
+    }
+
+    public long getElapsedTime(){
+        return endTime - startTime;
+    }
+
+}
